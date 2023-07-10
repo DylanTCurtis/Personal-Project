9f34b5578985127a3fab963a43805191e15ef6ae
DylanTCurtis <dylanthomascurtis@gmail.com>
2023-07-10 11:55:13 -0600

    The first push mate

---
+++ main/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Welcome Page';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Other components and content */}
+    </div>
+  );
+}
+
+export default App;
